7398972cedfcfc4ac0efd371ba4c7e96414c2abb
suricatoti <alexandredslima@gmail.com>
2021-07-06 09:37:24 -0300

    Contador de Pessoas

---
+++ android/app/src/main/kotlin/br/com/suricatoti/contador_pessoas/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.suricatoti.contador_pessoas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Contador de Pessoas",
+      home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _people = 0;
+  String _infoText = "Pode Entrar!";
+
+  void _changePeople(int value) {
+    setState(() {
+      _people += value;
+
+      if (_people <= 0) {
+        _infoText = "Lotado!";
+      } else if (_people >= 10) {
+        _infoText = "Casa cheia!";
+      } else {
+        _infoText = "Pode Entrar!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset("images/restaurant.jpg",
+            fit: BoxFit.cover, height: 1000.0),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Pessoas: $_people",
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextButton(
+                      onPressed: () {
+                        _changePeople(1);
+                      },
+                      child: Text("+1",
+                          style: TextStyle(
+                              fontSize: 40.0, color: Colors.white)))),
+              Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextButton(
+                      onPressed: () {
+                        _changePeople(-1);
+                      },
+                      child: Text("-1",
+                          style: TextStyle(
+                              fontSize: 40.0, color: Colors.white)))),
+            ]),
+            Text(_infoText,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 30.0))
+          ],
+        ),
+      ],
+    );
+  }
+}
